153b62668d41ac6da1ff3d6ef45339a64801576e
HelgeKeck <helgekeck@hotmail.com>
2022-02-08 13:50:21 +0100

    Update pam.py

--- klippy_extra/pam.py
+++ klippy_extra/pam.py
@@ -12,7 +12,6 @@ class PAM:
 
     def handle_connect(self):
         self.toolhead = self.printer.lookup_object('toolhead')
-        self.algo = self.bed_mesh.bmc.orig_config['algo']
         self.probe_x_step = float((self.bed_mesh.bmc.orig_config['mesh_max'][0] - self.bed_mesh.bmc.orig_config['mesh_min'][0]) / self.bed_mesh.bmc.orig_config['x_count'])
         self.probe_y_step = float((self.bed_mesh.bmc.orig_config['mesh_max'][1] - self.bed_mesh.bmc.orig_config['mesh_min'][1]) / self.bed_mesh.bmc.orig_config['y_count'])
 
@@ -34,11 +33,11 @@ class PAM:
         mesh_y1 = min(self.y1 + self.offset, self.bed_mesh.bmc.orig_config['mesh_max'][1])
         mesh_cx = max(3, int((mesh_x1 - mesh_x0) / self.probe_x_step))
         mesh_cy = max(3, int((mesh_y1 - mesh_y0) / self.probe_y_step))
-        if self.algo == 'bicubic':
+        if self.bed_mesh.bmc.orig_config['algo'] == 'bicubic':
             if mesh_cx < 4 or mesh_cy < 4:
                 mesh_cx = min(6, mesh_cx)
                 mesh_cy = min(6, mesh_cy)
-        elif self.algo == 'lagrange':
+        elif self.bed_mesh.bmc.orig_config['algo'] == 'lagrange':
             mesh_cx = min(6, mesh_cx)
             mesh_cy = min(6, mesh_cy)
         self.gcode.run_script_from_command('BED_MESH_CALIBRATE PROFILE=ratos mesh_min={0},{1} mesh_max={2},{3} probe_count={4},{5}'.format(mesh_x0, mesh_y0, mesh_x1, mesh_y1, mesh_cx, mesh_cy))
