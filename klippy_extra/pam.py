8c39382a5089bf54b06e031a731e8bbd26122901
HelgeKeck <helgekeck@hotmail.com>
2022-12-23 10:44:45 +0100

    left right priming added

--- klippy_extra/pam.py
+++ klippy_extra/pam.py
@@ -7,6 +7,7 @@ class PAM:
         self.gcode = self.printer.lookup_object('gcode')
         self.bed_mesh = self.printer.lookup_object('bed_mesh')
         self.offset = self.config.getfloat('offset', 0.)
+        self.optimus_prime = self.config.getboolean('optimus_prime', False)
         self.gcode.register_command('PAM', self.cmd_PAM, desc=("PAM"))
         self.gcode.register_command('MESH_CONFIG', self.cmd_MESH_CONFIG, desc=("MESH_CONFIG"))
         self.printer.register_event_handler("klippy:connect", self.handle_connect)
@@ -32,13 +33,71 @@ class PAM:
         mesh_y0 = max(self.y0 - self.offset, self.bed_mesh.bmc.orig_config['mesh_min'][1])
         mesh_x1 = min(self.x1 + self.offset, self.bed_mesh.bmc.orig_config['mesh_max'][0])
         mesh_y1 = min(self.y1 + self.offset, self.bed_mesh.bmc.orig_config['mesh_max'][1])
-        mesh_cx = max(3, int((mesh_x1 - mesh_x0) / self.probe_x_step))
-        mesh_cy = max(3, int((mesh_y1 - mesh_y0) / self.probe_y_step))
-        if self.bed_mesh.bmc.orig_config['algo'] == 'lagrange' or (self.bed_mesh.bmc.orig_config['algo'] == 'bicubic' and (mesh_cx < 4 or mesh_cy < 4)):
-            mesh_cx = min(6, mesh_cx)
-            mesh_cy = min(6, mesh_cy)
+        mesh_cx = max(3, int(0.5 + (mesh_x1 - mesh_x0) / self.probe_x_step))
+        mesh_cy = max(3, int(0.5 + (mesh_y1 - mesh_y0) / self.probe_y_step))
+        if self.bed_mesh.bmc.orig_config['algo'] == 'lagrange':
+            # Lagrange interpolation tends to oscillate when using more than 6 samples
+           mesh_cx = min(6, mesh_cx)
+           mesh_cy = min(6, mesh_cy)
+        elif self.bed_mesh.bmc.orig_config['algo'] == 'bicubic':
+            # Bicubic interpolation needs at least 4 samples on each axis
+            mesh_cx = max(4, mesh_cx)
+            mesh_cy = max(4, mesh_cy)
+        if self.optimus_prime == True:
+            self.set_priming_location(mesh_x0, mesh_y0, mesh_x1, mesh_y1)
         self.gcode.respond_raw("PAM v0.1.0 bed mesh leveling...")
-        self.gcode.run_script_from_command('BED_MESH_CALIBRATE PROFILE=ratos mesh_min={0},{1} mesh_max={2},{3} probe_count={4},{5} relative_reference_index=-1'.format(mesh_x0, mesh_y0, mesh_x1, mesh_y1, mesh_cx, mesh_cy))
+        self.gcode.run_script_from_command('BED_MESH_CALIBRATE PROFILE=pam mesh_min={0},{1} mesh_max={2},{3} probe_count={4},{5} relative_reference_index=-1'.format(mesh_x0, mesh_y0, mesh_x1, mesh_y1, mesh_cx, mesh_cy))
+
+    def set_priming_location(self, mesh_x0, mesh_y0, mesh_x1, mesh_y1):
+        ratos_gcode = self.printer.lookup_object('gcode_macro RatOS')
+
+        mesh_x0 = mesh_x0
+        mesh_y0 = mesh_y0
+        mesh_x1 = mesh_x1
+        mesh_y1 = mesh_y1
+
+        toolhead_min_x = max(0, self.toolhead.kin.axes_min.x)
+        toolhead_min_y = max(0, self.toolhead.kin.axes_min.y)
+        toolhead_max_x = self.toolhead.kin.axes_max.x
+        toolhead_max_y = self.toolhead.kin.axes_max.y
+
+        prime_offset_left = 35
+        prime_offset_right = 30
+        prime_offset_front = 15
+        prime_offset_back = 15
+
+        prime_x_dimension = 15
+        prime_y_dimension = 100
+
+        nozzle_priming = str(ratos_gcode.variables['nozzle_priming']).lower() 
+        if nozzle_priming == 'primeline':
+            prime_x_dimension = 2
+            prime_offset_left = 2
+            prime_offset_right = 2
+            prime_offset_front = 2
+            prime_offset_back = 2
+
+        prime_x = mesh_x1
+        if toolhead_max_x - mesh_x1 - prime_offset_right > prime_x_dimension:
+            # right
+            prime_x = mesh_x1 + prime_offset_right + (prime_x_dimension / 2)
+        elif mesh_x0 - toolhead_min_x - prime_offset_left > prime_x_dimension:
+            # left
+            prime_x = mesh_x0 - prime_offset_right - (prime_x_dimension / 2)
+
+        if nozzle_priming == 'primeblob':
+            prime_y = mesh_y0 + ((mesh_y1 - mesh_y0) / 2)
+        elif nozzle_priming == 'primeline':
+            prime_y = (mesh_y0 + ((mesh_y1 - mesh_y0) / 2)) - (prime_y_dimension / 2)
+            prime_y = max(toolhead_min_y, prime_y)
+
+        # prime_x = 'right'
+        # nozzle_prime_start_x = ratos_gcode.variables['nozzle_prime_start_x'] 
+        # if str(ratos_gcode.variables['nozzle_prime_start_x']).lower() == 'min':
+        #     prime_x = 'left'
+
+        ratos_gcode.variables['nozzle_prime_start_x'] = prime_x
+        ratos_gcode.variables['nozzle_prime_start_y'] = prime_y
 
 def load_config(config):
     return PAM(config)
