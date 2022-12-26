e600b2b102106c4d5e4f9d6abfc3f85dc7895634
HelgeKeck <helgekeck@hotmail.com>
2022-12-26 11:39:05 +0100

    update fix

--- klippy_extra/pam.py
+++ klippy_extra/pam.py
@@ -43,9 +43,9 @@ class PAM:
         if self.bed_mesh.bmc.orig_config['algo'] == 'lagrange' or (self.bed_mesh.bmc.orig_config['algo'] == 'bicubic' and (mesh_cx < 4 or mesh_cy < 4)):
             mesh_cx = min(6, mesh_cx)
             mesh_cy = min(6, mesh_cy)
-        if self.optimus_prime == True:
-            self.set_priming_location(mesh_x0, mesh_y0, mesh_x1, mesh_y1)
-        self.gcode.respond_raw("PAM v0.1.4 bed mesh leveling...")
+        # if self.optimus_prime == True:
+        #     self.set_priming_location(mesh_x0, mesh_y0, mesh_x1, mesh_y1)
+        self.gcode.respond_raw("PAM v0.1.6 bed mesh leveling...")
         self.gcode.run_script_from_command('BED_MESH_CALIBRATE PROFILE=pam mesh_min={0},{1} mesh_max={2},{3} probe_count={4},{5} relative_reference_index=-1'.format(mesh_x0, mesh_y0, mesh_x1, mesh_y1, mesh_cx, mesh_cy))
 
     def set_priming_location(self, mesh_x0, mesh_y0, mesh_x1, mesh_y1):
